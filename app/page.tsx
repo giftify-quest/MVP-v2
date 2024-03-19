5b6110aac15060181b02fe6e959eb85e947abdc9
barabanoveugene <barabanoveugene@gmail.com>
2024-03-19 17:37:59 +0100

    add component section title

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
-import styles from "./page.module.scss";
 import { SectionTitle } from "@/components/section-title/SectionTitle";
 
 export default function Home() {
   return (
-    <main className={styles.main}>{/* <SectionTitle></SectionTitle> */}</main>
+    <main>
+      {" "}
+      <SectionTitle
+        mainWord="main word"
+        secondWord="second word"
+        variant="purple"
+      />
+    </main>
   );
 }
--- components/section-title/SectionTitle.tsx
+++ components/section-title/SectionTitle.tsx
@@ -1,18 +1,31 @@
-import styles from "./style.module.scss";
+"use client";
 
+import styles from "./style.module.scss";
+import classNames from "classnames";
+
+type Variant = "purple" | "green";
 interface ISectionTitle {
   mainWord: string;
   secondWord?: string;
+  variant: Variant;
 }
 
 export const SectionTitle: React.FC<ISectionTitle> = ({
   mainWord,
   secondWord,
+  variant,
 }) => {
   return (
     <div className={styles.wrapper}>
-      <div></div>
-      <div></div>
+      <div
+        className={classNames(styles.mainWord, {
+          [styles.bg_green]: variant === "green",
+          [styles.bg_purple]: variant === "purple",
+        })}
+      >
+        <div className={styles.content}>{mainWord}</div>
+      </div>
+      {secondWord && <div className={styles.second_word}>{secondWord}</div>}
     </div>
   );
 };
