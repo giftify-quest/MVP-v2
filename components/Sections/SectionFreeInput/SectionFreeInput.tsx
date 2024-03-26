a3490292fd98b451b2ea40fa1984e2d84994ae08
barabanoveugene <barabanoveugene@gmail.com>
2024-03-26 16:28:16 +0100

    add SectionFreeInput

---
+++ components/Sections/SectionFreeInput/SectionFreeInput.tsx
@@ -0,0 +1,9 @@
+import { SectionTitle } from "@/components/reusableComponent/section-title/SectionTitle";
+
+export const SectionFreeInput = () => {
+  return (
+    <div>
+      <SectionTitle variant="purple" mainWord="need to get in props" />
+    </div>
+  );
+};
