c6700d51870a7e4128a136e9e6ec10d79be706a1
Oleksandr Buhai <oleksandr.buugay@gmail.com>
2024-06-01 13:38:14 +0300

    fix bugs first quest

--- components/reusableComponent/SectionTitle/SectionTitle.tsx
+++ components/reusableComponent/SectionTitle/SectionTitle.tsx
@@ -7,24 +7,57 @@ import classNames from "classnames";
 type Variant = "purple" | "green";
 interface ITitleSectionProps extends ITitleSection {
   variant: Variant;
+  isFirstQuest?: boolean;
 }
 
 export const SectionTitle: React.FC<ITitleSectionProps> = ({
   mainWord,
   secondWord,
   variant,
+  isFirstQuest,
 }) => {
   return (
-    <div className={styles.wrapper}>
-      <div
-        className={classNames(styles.mainWord, {
-          [styles.bg_green]: variant === "green",
-          [styles.bg_purple]: variant === "purple",
-        })}
-      >
-        <div className={styles.content}>{mainWord}</div>
-      </div>
-      {secondWord && <div className={styles.second_word}>{secondWord}</div>}
-    </div>
+    <>
+      {isFirstQuest ? (
+        <>
+          <div
+            style={{
+              fontSize: "60px",
+              color: "#b6b5b1",
+              fontWeight: "600",
+              alignSelf: "center",
+            }}
+          >
+            {secondWord}
+          </div>
+          <div className={styles.wrapper}>
+            <div
+              className={classNames(styles.mainWord, {
+                [styles.bg_green]: variant === "green",
+                [styles.bg_purple]: variant === "purple",
+              })}
+            >
+              <div className={styles.content}>{mainWord}</div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className={styles.wrapper}>
+            <div
+              className={classNames(styles.mainWord, {
+                [styles.bg_green]: variant === "green",
+                [styles.bg_purple]: variant === "purple",
+              })}
+            >
+              <div className={styles.content}>{mainWord}</div>
+            </div>
+            {secondWord && (
+              <div className={styles.second_word}>{secondWord}</div>
+            )}
+          </div>
+        </>
+      )}
+    </>
   );
 };
